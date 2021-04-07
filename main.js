b658f8d566cc530af8bde8128594c90f244ec68e
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-04-07 16:23:23 +0200

    object and logic created

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// Returns a random DNA base
 const returnRandBase = () => {
     const dnaBases = ['A', 'T', 'C', 'G']
-    return dnaBases[Math.floor(Math.random() * 4)] 
+    return dnaBases[Math.floor(Math.random() * [dnaBases.length])] 
   }
   
   // Returns a random single stand of DNA containing 15 bases
@@ -12,3 +11,25 @@ const returnRandBase = () => {
     }
     return newStrand
   }
+  
+  const pAequorFactory = (number,dnaBase) => {
+    return {
+      specimenNum: number,
+      dna: dnaBase,
+      mutate(){
+        // Mutations
+        const nucleobase = ['A', 'T', 'C', 'G']
+        nucleobase.splice(nucleobase.indexOf(this.dna[Math.floor(Math.random()* this.dna.length)]),1)
+        let mutatedBase = nucleobase[Math.floor(Math.random() * nucleobase.length)]
+        console.log(`Changed ${this.dna[Math.floor(Math.random() * this.dna.length)]} to ${mutatedBase}`)
+        return this.dna.splice(Math.floor(Math.random()* this.dna.length),1,mutatedBase) // Adding new Mutated String
+      }
+    } 
+      
+  }
+
+  const testMutation = pAequorFactory(1,mockUpStrand())
+console.log(testMutation.dna)
+
+testMutation.mutate()
+console.log(testMutation.dna)
